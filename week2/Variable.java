7d51fdae9802d13cf21475f408a28adfb7102630
노성민 <85987504+nohseongmin@users.noreply.github.com>
2025-03-13 11:46:05 +0900

    Create Variable.java

---
+++ week2/Variable.java
@@ -0,0 +1,27 @@
+package week2;
+
+public class Variable {
+	public static void main(String[] args) {
+		//자바 데이터 타입
+		//직접 값을 넣는 초기값 = 리터럴 ex)정수 리터럴
+		byte bData = 65; //1byte 정수형~
+		char cData = 65; //2byte
+		short sData = 65; //2byte
+		int iData = 65; //4byte
+		long lData = 65; //8byte
+		float fData = 65.12f; //4byte 실수형~ ( float형 뒤에는 f를 붙여줄것)
+		double dData = 65.12d; //8byte
+		boolean boolData = true; //1byte 불리안	
+		
+		System.out.println("bData:" + bData);
+		System.out.println("cData:" + cData);
+		System.out.println("sData:" + sData);
+		System.out.println("iData:" + iData);
+		System.out.println("lData:" + lData);
+		System.out.println("fData:" + fData);
+		System.out.println("dData:" + dData);
+		System.out.println("boolData:" + boolData);
+		
+		
+	}
+}
