961bab06a04669eb455789029d173b2c5183c458
노성민 <85987504+nohseongmin@users.noreply.github.com>
2025-03-20 12:08:34 +0900

    Create TwoOperand5.java

---
+++ week3/TwoOperand5.java
@@ -0,0 +1,19 @@
+package week3;
+
+public class TwoOperand5 {
+	public static void main(String[] args) {
+		//복합대입연산자 += -= *= /= 
+		int result = 10;
+		int result2 = result+10;
+		result = result + 30;
+		
+		//result2  = result + 10;
+		result+=10;
+		result -= 3;
+		result *= 5;
+		
+		result %= 4;
+		
+		
+	}
+}
